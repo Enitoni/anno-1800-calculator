4f0752ce8f3ffb8332e58998dfc871f111d42d73
unknown <enitoni@gmail.com>
2020-04-04 16:17:37 +0200

    add button system

---
+++ src/common/button/components/PrimaryButton.tsx
@@ -0,0 +1,40 @@
+import styled from "../../../modules/theming/custom"
+import { Button } from "./Button"
+import { FIELDSET_ELEMENT_HEIGHT } from "../../input/constants"
+import { getColor, getFontColor } from "../../../modules/theming/helpers"
+import { size } from "polished"
+
+export const PrimaryButton = styled(Button)`
+  display: inline-flex;
+
+  justify-content: center;
+  align-items: center;
+
+  padding: 16px;
+  height: ${FIELDSET_ELEMENT_HEIGHT};
+
+  background: ${getColor("accent")};
+  border-radius: 4px;
+  color: ${getColor("primary")};
+
+  transition: 200ms ease;
+  transition-property: color background;
+
+  > .label {
+    font-weight: 700;
+    font-size: 13px;
+    text-transform: uppercase;
+  }
+
+  > .icon {
+    ${size(16)};
+    fill: ${getColor("primary")};
+
+    margin-right: 8px;
+  }
+
+  &:hover {
+    background: ${getFontColor("normal")};
+    color: ${getColor("primary")};
+  }
+`
