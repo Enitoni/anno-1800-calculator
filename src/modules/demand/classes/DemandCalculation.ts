3401c86e8dd13de37d58ba188b06b9ea0819581f
unknown <enitoni@gmail.com>
2020-04-04 16:01:00 +0200

    add a sidebar to DemandPage

---
+++ src/modules/demand/classes/DemandCalculation.ts
@@ -0,0 +1,3 @@
+export class DemandCalculation {
+  constructor(public name: string) {}
+}
--- src/modules/demand/components/DemandPage.tsx
+++ src/modules/demand/components/DemandPage.tsx
@@ -1,11 +1,24 @@
 import React from "react"
 import { PageTitle } from "../../core/components/PageTitle"
+import styled from "../../theming/custom"
+import { Section } from "../../core/components/Section"
+import { SIDEBAR_WIDTH } from "../../core/constants"
+
+const Container = styled.div`
+  display: flex;
+`
+
+const Sidebar = styled(Section)`
+  max-width: ${SIDEBAR_WIDTH}px;
+`
 
 export function DemandPage() {
   return (
     <>
       <PageTitle title="Resident demands" icon="box" />
-      <span>lol whoever reading this gay af</span>
+      <Container>
+        <Sidebar label="Your islands"></Sidebar>
+      </Container>
     </>
   )
 }
