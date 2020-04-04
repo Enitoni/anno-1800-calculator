68a9f962741470f550c2e6329d676b0913ab03df
unknown <enitoni@gmail.com>
2020-04-04 20:11:28 +0200

    start demandStore with existing calculation

--- src/modules/demand/stores/demandStore.ts
+++ src/modules/demand/stores/demandStore.ts
@@ -3,8 +3,8 @@ import { observable } from "mobx"
 import { DemandCalculation } from "../classes/DemandCalculation"
 
 export class DemandStore implements InitializableStore {
-  @observable public calculations: DemandCalculation[] = []
-  @observable public selected?: DemandCalculation
+  @observable public calculations: DemandCalculation[] = [new DemandCalculation()]
+  @observable public selected: DemandCalculation = this.calculations[0]
 
   public init() {}
 
