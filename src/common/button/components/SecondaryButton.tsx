cff38e5b3b51348a6c3be2cf4587503b240db3f5
unknown <enitoni@gmail.com>
2020-04-05 20:42:46 +0200

    add SecondaryButton

---
+++ src/common/button/components/SecondaryButton.tsx
@@ -0,0 +1,45 @@
+import styled from "../../../modules/theming/custom"
+import { Button } from "./Button"
+import { FIELDSET_ELEMENT_HEIGHT } from "../../input/constants"
+import { getColor, getFontColor, getTransparency } from "../../../modules/theming/helpers"
+import { size } from "polished"
+
+export const SecondaryButton = styled(Button)`
+  display: inline-flex;
+
+  justify-content: center;
+  align-items: center;
+
+  padding: 16px;
+  height: ${FIELDSET_ELEMENT_HEIGHT};
+
+  background: ${getTransparency("lightPositive")};
+  border-radius: 4px;
+  color: ${getFontColor("normal")};
+
+  transition: 200ms ease;
+  transition-property: color background;
+
+  > .label {
+    font-weight: 700;
+    font-size: 0.75em;
+    text-transform: uppercase;
+  }
+
+  > .icon {
+    ${size(16)};
+    fill: ${getFontColor("normal")};
+
+    margin-right: 8px;
+    transition: 200ms ease fill;
+  }
+
+  &:hover {
+    background: ${getFontColor("normal")};
+    color: ${getColor("primary")};
+
+    > .icon {
+      fill: ${getColor("primary")};
+    }
+  }
+`
