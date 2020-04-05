c6887d53c54c44708210109425b9ab860648946c
unknown <enitoni@gmail.com>
2020-04-05 20:10:50 +0200

    add more transparencies

--- src/modules/theming/themes.ts
+++ src/modules/theming/themes.ts
@@ -13,5 +13,7 @@ export const darkTheme: Theme = {
   },
   transparencies: {
     lightPositive: "rgba(255, 255, 255, 0.1)",
+    lightNegative: "rgba(0, 0, 0, 0.3)",
+    strongNegative: "rgba(0, 0, 0, 0.6)",
   },
 }
--- src/modules/theming/types/Theme.ts
+++ src/modules/theming/types/Theme.ts
@@ -11,5 +11,7 @@ export type Theme = {
   }
   transparencies: {
     lightPositive: string
+    lightNegative: string
+    strongNegative: string
   }
 }
