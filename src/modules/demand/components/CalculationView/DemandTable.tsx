701ae8379d510247a17ef7d5e8792d177b74241d
unknown <enitoni@gmail.com>
2020-04-05 16:25:35 +0200

    Add images and filter box in DemandTable

--- src/modules/demand/components/CalculationView/DemandTable.tsx
+++ src/modules/demand/components/CalculationView/DemandTable.tsx
@@ -1,33 +1,82 @@
 import { DemandCalculation } from "../../classes/DemandCalculation"
 import { useObserver } from "mobx-react-lite"
 import { Table } from "../../../../common/dom/components/Table"
-import React from "react"
+import React, { useState } from "react"
+import styled from "../../../theming/custom"
+import { size } from "polished"
+import { slugify } from "../../../../common/lang/string/slugify"
+import { getTransparency } from "../../../theming/helpers"
+import { TextInput } from "../../../../common/input/components/TextInput"
 
 export type DemandTableProps = {
   calculation: DemandCalculation
 }
 
+const NameContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const Image = styled.img`
+  ${size(32)};
+  border-radius: 100%;
+  padding: 4px;
+  background: ${getTransparency("lightPositive")};
+`
+
+const Name = styled.span`
+  margin-left: 16px;
+`
+
+const FilterInput = styled(TextInput)`
+  margin-bottom: 8px;
+  width: 100%;
+`
+
 export function DemandTable(props: DemandTableProps) {
   const { calculation } = props
+  const [filter, setFilter] = useState("")
 
-  return useObserver(() => (
-    <Table>
-      <Table.Head>
-        <Table.Row>
-          <Table.HeadColumn>Resource</Table.HeadColumn>
-          <Table.HeadColumn>Name</Table.HeadColumn>
-          <Table.HeadColumn>Consumption</Table.HeadColumn>
-        </Table.Row>
-      </Table.Head>
-      <Table.Body>
-        {calculation.demand.map((demand, i) => (
-          <Table.Row key={demand.name + i}>
-            <Table.Data></Table.Data>
-            <Table.Data>{demand.name}</Table.Data>
-            <Table.Data>{demand.consumption.toFixed(4)}</Table.Data>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
-  ))
+  return useObserver(() => {
+    const { demand } = calculation
+
+    if (demand.length === 0) {
+      return <span>Tweak the population controls above to see results.</span>
+    }
+
+    const filtered = filter
+      ? demand.filter((d) => d.name.toLowerCase().includes(filter.toLowerCase()))
+      : demand
+
+    return (
+      <>
+        <FilterInput
+          placeholder="Filter results..."
+          value={filter}
+          onInput={(v) => setFilter(v)}
+        />
+        <Table>
+          <Table.Head>
+            <Table.Row>
+              <Table.HeadColumn>Name</Table.HeadColumn>
+              <Table.HeadColumn>Consumption</Table.HeadColumn>
+            </Table.Row>
+          </Table.Head>
+          <Table.Body>
+            {filtered.map((demand, i) => (
+              <Table.Row key={demand.name + i}>
+                <Table.Data>
+                  <NameContainer>
+                    <Image src={`/img/resources/${slugify(demand.name)}.png`} />
+                    <Name>{demand.name}</Name>
+                  </NameContainer>
+                </Table.Data>
+                <Table.Data>{demand.consumption.toFixed(4)}</Table.Data>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </>
+    )
+  })
 }
