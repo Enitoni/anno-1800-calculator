0b8202c4b89f74e45e93ad17668a0e372131a172
unknown <enitoni@gmail.com>
2020-04-05 20:46:13 +0200

    this should never have been commited

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,12 @@ import { App } from "./modules/core/components/App"
 
 import { createManager } from "./common/state/manager"
 import { ManagerContext } from "./common/state/contexts/ManagerContext"
-import { SaveCalculationsModal } from "./modules/demand/components/SaveCalculationsModal"
 
 async function main() {
   const manager = createManager()
   const element = document.querySelector(".app")
 
   await manager.init()
-
-  manager.stores.modalStore.spawn({
-    key: "e",
-    render: () => <SaveCalculationsModal />,
-  })
 
   ReactDOM.render(
     <ManagerContext.Provider value={manager}>{<App />}</ManagerContext.Provider>,
