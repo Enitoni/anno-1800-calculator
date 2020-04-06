373fcc2347d61a20f1e605a35e29faec43101d53
unknown <enitoni@gmail.com>
2020-04-06 18:24:42 +0200

    apparently I'm dumb

--- src/common/routing/hooks/useRouteLink.ts
+++ src/common/routing/hooks/useRouteLink.ts
@@ -5,8 +5,8 @@ import UrlPattern from "url-pattern"
 export const useRouteLink = (to: string, activeTo = to) => {
   const { routingStore } = useStores()
 
-  const safeTo = `${__webpack_public_path__}/${to}`
-  const safeActiveTo = `${__webpack_public_path__}/${activeTo}`
+  const safeTo = `${__webpack_public_path__}${to}`
+  const safeActiveTo = `${__webpack_public_path__}${activeTo}`
 
   const click = (event: React.MouseEvent) => {
     event.preventDefault()
