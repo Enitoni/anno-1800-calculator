131b01d0124fcebbdac249449a2624b374cbb498
unknown <enitoni@gmail.com>
2020-05-18 11:09:14 +0200

    automatically select category when active changes

--- src/modules/chain/components/ChainCategoryList.tsx
+++ src/modules/chain/components/ChainCategoryList.tsx
@@ -1,14 +1,26 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { BuildingCategoryItem } from "./BuildingCategoryItem"
 import { categories } from "../categories"
 import { selectedCategoryContext } from "../contexts/selectedCategoryContext"
 import { ChainCategory } from "../types/ChainCategory"
+import { useObserver } from "mobx-react-lite"
+import { useStores } from "../../../common/state/hooks/useStores"
 
 const { Provider } = selectedCategoryContext
 
 export function ChainCategoryList() {
   const [category, setCategory] = useState<ChainCategory | undefined>(undefined)
   const context = { category, select: (c: ChainCategory) => setCategory(c) }
+
+  const { chainStore } = useStores()
+
+  const activeCategory = useObserver(() =>
+    categories.find((c) => c.buildings.some((b) => b === chainStore.selected)),
+  )!
+
+  useEffect(() => {
+    setCategory(activeCategory)
+  }, [activeCategory])
 
   return (
     <Provider value={context}>
